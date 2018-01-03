e9dda800e643251abba49bf0b7414c9b3292793f
jimmy2752@gmail.com <jimmy2752@gmail.com>
2018-01-03 23:24:11 +0800

    save_to_db_ok

--- pymongo_imp.py
+++ pymongo_imp.py
@@ -25,17 +25,32 @@ def get_collection(db):
     #print db.collection_names()     #打印集合名字
     return coll                     #返回集合
 
-def insert_one_doc(db,file_name0,filesize0,magnet0,keywords0):
+def insert_one_doc(db,file_name0,filesize0,magnet0):
     # 插入一个document               #mongodb中每一条信息叫document
     coll = db['informations']       #选择这个集合
     #step1 获取magnet链接的keyid   21-61位为关键字串   [20:61]
     keywords0=magnet0[20:60]
     #step2 查找keywords0是否重复。
     if coll.find_one({"Vedio_KeyID": keywords0}) == None:
-        pass
-    else:
-        information = {"Vedio_name": file_name0, "File_size": filesize0,"Magnet_Link": magnet0,"Save_Time": datetime.datetime.utcnow(),"Vedio_KeyID": keywords0}     #字典，准备插入的字典。
-        information_id = coll.insert(information)         #插入这一条字典，获取
+        print "数据库中没有该文件。Will Add to the database!"
+        information = {"Vedio_name": file_name0, "File_size": filesize0, "Magnet_Link": magnet0,
+                       "Save_Time": datetime.datetime.utcnow(), "Vedio_KeyID": keywords0}  # 字典，准备插入的字典。
+        information_id = coll.insert(information)  # 插入这一条字典，获取
         print information_id
+    else:
+        print "数据库已经有该文件。忽略!"
+        pass
+        #print "else"
+
 if __name__ == '__main__':
     print "Please use it by import!"
+    db = get_db()  # 建立链接
+    my_collection = get_collection(db)  # 获取集合
+    post = {"author": "Mike", "text": "My first blog post!", "tags": ["mongodb", "python", "pymongo"],
+            "date": datetime.datetime.utcnow()}  # 设置需要插入的内容，为一个字典。
+    # 插入记录
+    my_collection.insert(post)  # 插入上面的字典
+    print my_collection.find_one({"x": "10"})
+    information = {"name": "quyang", "age": "25"}     #字典，准备插入的字典。
+    information_id = my_collection.insert(information)         #插入这一条字典，获取
+    print information_id
--- torrent_k3.py
+++ torrent_k3.py
@@ -1,14 +1,4 @@
 # -*- coding: utf-8 -*-
-import urllib2
-import urllib
-from lxml import etree
-
-###########################华丽分割线
-import codecs
-
-import requests
-from bs4 import BeautifulSoup
-
 '''
     一定要注意从网上粘贴过来的代码需要重新调整一下缩进！！！
     本代码是python2的代码，python3不适用。
@@ -16,7 +6,7 @@ from bs4 import BeautifulSoup
     #任务2:使用代理，而不是使用全局代理。使得其他软件的使用不受影响。
     #任务3:根据搜索内容，转换为正常字符，命名文件名保存，而不是每次都是同一个文件名。     <<<<<<OK!>>>>>>
     #任务4:每20个链接保存一个文件，之后另增加文件保存。                               <<<<<<OK!>>>>>>
-    #任务5:将抓取内容存放到MySQL或者其他简单数据库。
+    #任务5:将抓取内容存放到MySQL或者其他简单数据库。Monogodb
     #任务6:增加用户输入，然后搜索用户输入的内容。                                    <<<<<<OK!>>>>>>
     #任务7:使用beautifulsoup方法实现抓取。实现高级筛选功能。                         <<<<<<OK!>>>>>>
 '''
@@ -26,6 +16,15 @@ from bs4 import BeautifulSoup
 	BS的语法确实比xpath更加简洁容易懂，理清思路，分步一步一步循序渐进，就能够得到解决方案。
 
 '''
+import urllib2
+import urllib
+from lxml import etree
+###########################华丽分割线
+import codecs
+import requests
+from bs4 import BeautifulSoup
+
+import pymongo_imp
 
 url = 'https://www.torrentkitty.tv/search/'
 
@@ -33,16 +32,13 @@ keys2x = raw_input("请输入搜索关键字：")
 
 keyword = urllib.quote(keys2x)  # 这是python2的语法
 
-pages = 20
+pages = 80
 
-# os.chdir('/Users/llm/PycharmProjects/')
+
 
 file_name = '/Users/llm/PycharmProjects/' + keys2x + '.txt'
 
 ks = file_name
-
-
-
 
 for page in range(0, pages):
     page = str(page)
@@ -53,12 +49,8 @@ for page in range(0, pages):
 
     h.add_header('User-Agent',
                  'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.8.1.14) Gecko/20080404 (FoxPlus) Firefox/2.0.0.14')
-
-
-
     ht = urllib2.urlopen(h)
     html = ht.read(ht)
-
     soup = BeautifulSoup(html, "lxml")                                              #####>>>>>>1     创建美丽汤。
     movie_list_soup = soup.find('table', attrs={'id': 'archiveResult'})             #####>>>>>>2     定位到列表所在位置。#archiveResult
     movie_name_list = []                                                            #####>>>>>>3     新建列表，存放查找内容。
@@ -66,21 +58,16 @@ for page in range(0, pages):
     for movie_li in movie_list_soup.find_all('tr'):                                 #####>>>>>>4     设置过滤器。找到所有tr标签。
         search_flag=search_flag+1
         if search_flag > 1:                                                         #####>>>>>>4.1   设置查找标志，跳过第一个，因为第一个是表头。
-
             detail = movie_li.find('td', attrs={'class': 'size'}).getText()         #####>>>>>>5     定位所有的size标签，得到字符串。
             #上面获取了文件大小。🈶️❕
-
             #print "%s \n" % detail
             if movie_li.find('a', attrs={'rel': 'magnet'}) is None:
                 pass
             else:
-
                 detail_name1 = movie_li.find('a', attrs={'rel': 'magnet'})['title']
                 FHD_flag=detail_name1.find('FHD')
                 Thz_flag=detail_name1.find('Thz.la')
             if FHD_flag<>-1 or Thz_flag<>-1:                                         #####>>>>>>5.1     如果是高清或者是tha.lz那么就存档。
-
-
             ##if detail.find('mb')<>-1:                                               #####>>>>>>6     如果文件大小满足要求，那么下一步寻找兄弟节点。
                 #文件名称
                 if movie_li.find('a', attrs={'rel': 'magnet'}) is None:
@@ -89,7 +76,6 @@ for page in range(0, pages):
                     detail_name=movie_li.find('a', attrs={'rel': 'magnet'})['title']
                     #上面获取了文件名称。❕
                     print detail_name
-
                 # 文件大小  detail
                 print detail
                 #链接地址
@@ -100,23 +86,26 @@ for page in range(0, pages):
                     detail_mag=movie_li.find('a', attrs={'rel': 'magnet'})['href']      #####>>>>>>7     获取磁力链接地址。
                     #上面获取了磁力链接。❕
                     print detail_mag
+                    #with open(file_name, 'a') as p:  # '''Note'''：Ａppend mode, run only once!
+                    #    p.write("%s \n \n" % detail_mag)  ##!!encode here to utf-8 to avoid encoding
 
-                    with open(file_name, 'a') as p:  # '''Note'''：Ａppend mode, run only once!
-                        p.write("%s \n \n" % detail_mag)  ##!!encode here to utf-8 to avoid encoding
+                    #获取了磁力链接之后开始存入数据库。
+                    print "开始进行mongodb数据库操作:"
+                    #存入数据库
+                    db=pymongo_imp.get_db()
+                    my_collection = pymongo_imp.get_collection(db)
+                    #
+                    pymongo_imp.insert_one_doc(db,detail_name,detail,detail_mag)
+                    #print detail
+                    #print detail_name
+                    #print detail_mag
+                    #
+                    print "截止目前，数据库中存放条目数量：%s个" % int(my_collection.count())
 
 
 
 
 
-
-    #file_name = ks + '_' + page  # 让文件按页码保存，避免一个文件中链接数量太多。
-
-
-    #with open(file_name, 'a') as p:  # '''Note'''：Ａppend mode, run only once!
-
-       # for mag in mags:
-
-            #p.write("%s \n \n" % (mag.attrib['href']))  ##!!encode here to utf-8 to avoid encoding
 
 
 
